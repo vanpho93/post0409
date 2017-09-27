7ffea50557f217565568346ebec22bf3f1bed80a
Van Pho Nguyen <vanpho02@gmail.com>
2017-09-27 18:22:55 +0700

    form

--- index.js
+++ index.js
@@ -11,9 +11,20 @@ let arrPerson = [
 app.set('view engine', 'ejs');
 app.set('views', './views');
 
+// app.locals.name = 'Khoa Pham Online';
+// app.locals.address = '92 Le Thi Rieng';
+
+app.locals = {
+    name: 'Khoa Pham Online',
+    address: '92 Le Thi Rieng'
+};
+app.locals.district = 'Q1';
+
 app.use(express.static('./public'));
 
 app.get('/', (req, res) => res.render('home', { arrPerson }));
+
+app.get('/signin', (req, res) => res.render('signin'));
 
 reload(app);
 
