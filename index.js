da08831a0728d49232b7c9f8a14e832387b33e53
Van Pho Nguyen <vanpho02@gmail.com>
2017-09-27 17:49:11 +0700

    show an array

--- index.js
+++ index.js
@@ -2,12 +2,18 @@ const express = require('express');
 const reload = require('reload');
 const app = express();
 
+let arrPerson = [
+    { name: 'Ti', age: 10 },
+    { name: 'Teo', age: 11 },
+    { name: 'Tun', age: 13 },
+];
+
 app.set('view engine', 'ejs');
 app.set('views', './views');
 
 app.use(express.static('./public'));
 
-app.get('/', (req, res) => res.render('home'));
+app.get('/', (req, res) => res.render('home', { arrPerson }));
 
 reload(app);
 
